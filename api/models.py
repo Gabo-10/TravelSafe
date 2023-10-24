adabb81b1adb4624df5edf6b2da76ea99a0c0e9b
Gabo <gabrielm1877@gmail.com>
2023-10-24 15:51:45 -0600

    Segundo respaldo

---
+++ api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django import forms
+
+         # Create your models here.
+
+# class Genero(models.Model):
+#     idGenero = models.IntegerField(primary_key=True, db_column='idGenero')
+#     tipoGenero = models.CharField(max_length=100,db_column='tipoGenero')
+#     class Meta:
+#         db_table='Genero'
+        
+# class Alumno(models.Model):
+#     idAlumnos = models.IntegerField(primary_key=True,db_column='idAlumno')
+#     nameAlumno = models.CharField(max_length=100,db_column='nameAlumno')
+#     fk_genero = models.ForeignKey(Genero,default=1,on_delete=models.CASCADE,db_column='fk_genero')
+#     class Meta:
+#         db_table='Alumnos'
+        
+# class Alumno_has_Genero(models.Model):
+#     fk_Alumno = models.ForeignKey(Alumno,default=1,on_delete=models.CASCADE,db_column='fk_alumno')
+#     fk_Genero = models.ForeignKey(Genero,default=1,on_delete=models.CASCADE,db_column='fk_genero')
+#     class Meta:
+#         db_table='alumno_has_genero'
+
+
+class Usuarios(models.Model):
+    idUsuarios = models.IntegerField(primary_key=True, db_column='id')
+    usuario = models.CharField(max_length=100,db_column='usuario')
+    contraseña = models.CharField(max_length=100,db_column='contraseña')
+    correo = models.CharField(max_length=100,db_column='correo')
+    class Meta:
+        db_table='Usuarios'
+        
+        
+class Encuesta(models.Model):
+    correo = models.CharField(max_length=100, db_column='correo')
+    nombre = models.CharField(max_length=100, db_column='nombre')
+    pregun1 = models.CharField(max_length=100, db_column='pregun1')
+    pregun2 = models.CharField(max_length=100, db_column='pregun2')
+    pregun3 = models.CharField(max_length=100, db_column='pregun3')
+    pregun4 = models.CharField(max_length=100, db_column='pregun4')
+    pregun5 = models.CharField(max_length=100, db_column='pregun5')
+    pregun6 = models.CharField(max_length=100, db_column='pregun6')
+    pregun7 = models.CharField(max_length=100, db_column='pregun7')
+    pregun8 = models.CharField(max_length=100, db_column='pregun8')
+    pregun9 = models.CharField(max_length=100, db_column='pregun9')
+    pregun10 = models.CharField(max_length=100, db_column='pregun10')
+    class Meta:
+        db_table='formula'
---
+++ api/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from rest_framework.views import APIView
+from django.contrib.auth import login
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import SuspiciousOperation
+from .models import Usuarios
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+
+
+
+class Home(APIView):
+    template_name="login.html"
+    def get(self,request):
+        return render(request,self.template_name)
+    
+class Inicio(APIView):
+    template_name="index.html"
+    def get(self,request):
+        return render(request,self.template_name) 
+    
+# class Usuario:
+#     def Usuari(self, nombre, correo, contraseña):
+#         self.nombre = nombre
+#         self.correo = correo
+#         self.contraseña = contraseña  
+# hasta 
+    
+def registro(request):
+    if request.method == 'POST':
+        nom = request.POST['usuario']
+        contra = request.POST['pass']
+        corr = request.POST['email']
+        
+        # Verificar si el correo ya está registrado
+        if Usuarios.objects.filter(correo=corr).exists():
+            messages.error(request, 'El correo ya está registrado')
+            return render(request, 'login.html')  # Cambia 'registro.html' al nombre correcto de tu página de registro
+        
+        Usuarios(usuario=nom,  correo=corr, contraseña=contra).save()
+        messages.success(request, 'Usuario registrado exitosamente')
+
+        # Verificacion de correo
+        subject = 'Verficación del correo!'
+        message = f"!Gracias por unirte a nuestra página. Por favor haz click en el siguiente enlace para verificar tu correo! "
+        from_email = "gabrielm1877@gmail.com"
+        send_mail(subject, message, from_email, [corr])
+        messages.success(request, "Usuario registrado correctamente, Por favor verifica tu correo!")
+        return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+ 
+ #----------------------------------------------------------------       
+        
+def inicio_de_sesion(request):
+    if request.method == 'POST':
+        correo1   = request.POST.get('correoFo')
+        contraseña1  = request.POST.get('passFo')
+        
+        try:
+            user = Usuarios.objects.get(correo=correo1, contraseña=contraseña1)
+            request.session['correo'] = user.correo
+
+            return redirect('index')
+        except Usuarios.DoesNotExist:
+            messages.error(request, 'User does not exist!')
+        except Usuarios.MultipleObjectsReturned:
+            messages.error(request, 'Multiples users with the same name!')
+
+    return render(request, 'login.html')
+      
