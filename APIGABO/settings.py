5a8d1bd37c820a3cfc58ba0b1a3f06986df3d522
Gabo <gabrielm1877@gmail.com>
2023-11-27 17:37:30 -0600

    Hosts

--- APIGABO/settings.py
+++ APIGABO/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-)5a+-b7ec0u3#10n3ydp@r^itbvzm*r^_7#w&2&4df2o%dyxc6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
